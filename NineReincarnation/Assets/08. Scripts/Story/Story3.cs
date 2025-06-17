0e5efcfe7963f3959102a8b82fd97261ef03b2a1
helIok1tty <139304904+helIok1tty@users.noreply.github.com>
2025-06-17 18:18:35 +0900

    feat: story3
    1. 텍스트 사운드
    2. m키 속도, 사이즈, 위치 조절

--- NineReincarnation/Assets/08. Scripts/Story/Story3.cs
+++ NineReincarnation/Assets/08. Scripts/Story/Story3.cs
@@ -97,6 +97,9 @@ public class Story3 : Story
     {
         switch (eventMarker.name)
         {
+            case "sound":
+                DialogueManager.Instance.PlayTextSound();
+                break;
             case "event1_6":
                 StoryManager.Instance.eventObj["혼령2"]?.StartAnim("Ghost_Laughing");
                 break;
@@ -256,10 +259,10 @@ public class Story3 : Story
         _mKey.SetActive(true);
         while (true)
         {
-            _mKey.transform.localScale = new Vector3(0.5f, 0.5f, 1f);
-            yield return new WaitForSeconds(1f);
-            _mKey.transform.localScale = new Vector3(0.5f, 0.4f, 1f);
-            yield return new WaitForSeconds(1f);
+            _mKey.transform.localScale = new Vector3(0.3f, 0.3f, 1f);
+            yield return new WaitForSeconds(0.5f);
+            _mKey.transform.localScale = new Vector3(0.3f, 0.2f, 1f);
+            yield return new WaitForSeconds(0.5f);
         }
     }
     #endregion
