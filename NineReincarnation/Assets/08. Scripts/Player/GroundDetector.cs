504fdd9067b31c4ad275fdc2a1e7e0df8a755e77
kdsh627 <105626103+kdsh627@users.noreply.github.com>
2025-07-10 19:38:58 +0900

    fix : 경사면 오를때 물리 로직 추가
    - 경사면을 오를때는 움직임 공식을 다르게 적용

--- NineReincarnation/Assets/08. Scripts/Player/GroundDetector.cs
+++ NineReincarnation/Assets/08. Scripts/Player/GroundDetector.cs
@@ -1,58 +1,97 @@
 using Player.Controller;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.UI.Image;
 
 public class GroundDetector : MonoBehaviour
 {
     [Header("--- 플레이어 컨트롤러 ---")]
     [SerializeField] private PlayerController player;
 
-    private bool isResetJump = false;
-    private Collider2D prevCollider;
     private int _groundCount = 0;
+
 
     private void Init()
     {
+        player.IsGround = true;
+        _groundCount++;
         player.ResetJumpCount();
-        isResetJump = true;
     }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Ground"))
+        if (collision.CompareTag("Slope"))
         {
-            _groundCount++;
-            player.IsGround = true;
-            Init();
-        }
-        if (collision.CompareTag("Platform"))
-        {
-            _groundCount++;
+            if (player.Rb2d.linearVelocityY < 0.1f)
+            {
+                player.IsSlope = true;
+                Init();
+            }
         }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.CompareTag("Platform"))
+        if (collision.CompareTag("Ground"))
         {
-            if (isResetJump) return;
-            if (Mathf.Abs(player.Rb2d.linearVelocityY) > float.Epsilon) return;
+            if (player.IsGround)
+            {
+                return;
+            }
 
-            player.IsGround = true;
-            Init();
+            if (Mathf.Abs(player.Rb2d.linearVelocityY) <= 0.1f)
+            {
+                player.IsGround = true;
+                Init();
+            }
         }
+        else if (collision.CompareTag("Slope"))
+        {
+            if (player.IsSlope)
+            {
+                CalculateSlopeVector();
+                return;
+            }
 
+            if (player.Rb2d.linearVelocityY <= 0.1f)
+            {
+                player.IsSlope = true;
+                Init();
+            }
+        }
     }
 
+    private void CalculateSlopeVector() //경사진 곳의 경사 벡터를 구함
+    {
+        Vector2 origin = transform.position;
+        float distance = 1.0f;
+        LayerMask groundMask = LayerMask.GetMask("Ground");
+        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, distance, groundMask);
+
+        if (hit.collider != null)
+        {
+            Vector2 groundNormal = hit.normal;
+            player.SlopeDir = -Vector2.Perpendicular(groundNormal); //반시게 방향으로 90도 회전
+            Debug.DrawRay(hit.point, player.SlopeDir, Color.green, 1f);
+        }
+    }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Ground") || collision.CompareTag("Platform"))
+        if (collision.CompareTag("Ground") || collision.CompareTag("Slope"))
         {
-            _groundCount--;
-            if (_groundCount == 0)
+            if (collision.CompareTag("Slope"))
             {
+                player.IsSlope = false;
+                player.SlopeDir = Vector2.right;
+            }
+
+            _groundCount--;
+            if (_groundCount <= 0)
+            {
+                _groundCount = 0;
                 player.IsGround = false;
-                isResetJump = false;
             }
         }
     }
--- NineReincarnation/Assets/08. Scripts/Player/PlayerController.cs
+++ NineReincarnation/Assets/08. Scripts/Player/PlayerController.cs
@@ -34,6 +34,7 @@ namespace Player.Controller
         [SerializeField] private bool _isLook = false; //플레이어가 줌을 실행하고 있는가 판별
         [SerializeField] private bool _isDead = false; //플레이어가 죽었는가 판별
         [SerializeField] private bool _isBusy = false;
+        [SerializeField] private bool _isSlope = false;
 
         private int _jumpCount = 0; //더블 점프 제어
         private PlayerDirection _direction; //플레이어 방향
@@ -44,6 +45,7 @@ namespace Player.Controller
         private Collider2D _collider;
         private OneWayPlatform _oneWayPlatform;
         private PlayerAnimationState _currentState;
+        private Vector2 _slopeDir = Vector2.right; //경사면 이동을 위한 벡터
 
         public int JumpCount => _jumpCount;
         public PlayerAnimationState CurrentState => _currentState;
@@ -88,6 +90,18 @@ namespace Player.Controller
         {
             get => _isBusy;
             set => _isBusy = value;
+        }
+
+        public bool IsSlope
+        {
+            get => _isSlope;
+            set => _isSlope = value;
+        }
+
+        public Vector2 SlopeDir
+        {
+            get => _slopeDir;
+            set => _slopeDir = value;
         }
 
         private void Awake()
@@ -170,7 +184,15 @@ namespace Player.Controller
             {
                 SetStop();
             }
-            _rb2d.linearVelocityX = (int)_direction * _speed;
+
+            if (IsSlope)
+            {
+                _rb2d.linearVelocity = (int)_direction * _speed * _slopeDir;
+            }
+            else
+            {
+                _rb2d.linearVelocityX = (int)_direction * _speed;
+            }
         }
 
         /// <summary>
@@ -179,6 +201,10 @@ namespace Player.Controller
         public void Jump()
         {
             if (_jumpCount >= 2) return;
+
+            _isSlope = false;
+            _isGround = false;
+            _slopeDir = Vector2.right;
 
             _rb2d.linearVelocityY = 0;
 
