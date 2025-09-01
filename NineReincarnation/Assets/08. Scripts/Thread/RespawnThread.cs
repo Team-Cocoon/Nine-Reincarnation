b6cb3450de2b8097b31e3625a110693fa230ee17
helIok1tty <139304904+helIok1tty@users.noreply.github.com>
2025-09-01 12:48:37 +0900

    fix : thread collider

--- NineReincarnation/Assets/08. Scripts/Thread/RespawnThread.cs
+++ NineReincarnation/Assets/08. Scripts/Thread/RespawnThread.cs
@@ -25,6 +25,7 @@ public class RespawnThread : Thread, ICollidable
     [SerializeField] private bool _isHit;
     [SerializeField] private float _disappearTime = 0f;
     [SerializeField] private float _respawnTime = 0f;
+    [SerializeField] private float _margin = 0.5f;
 
     //private List<GameObject> _playersOnRope = new List<GameObject>();
     private Coroutine _disappearCoroutine;
@@ -50,7 +51,7 @@ public class RespawnThread : Thread, ICollidable
         }
         _lineRenderer.positionCount = _segmentPositions.Length;
         _lineRenderer.SetPositions(_segmentPositions);
-        _edgeCollider.edgeRadius = threadWidth * 0.5f;
+        _edgeCollider.edgeRadius = threadWidth * _margin;
         _edgeCollider.points = _colliderPositions;
     }
     /// <summary>
