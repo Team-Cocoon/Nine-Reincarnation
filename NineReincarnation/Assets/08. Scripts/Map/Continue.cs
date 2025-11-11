3b4efde94ece673af8d953b3a5fb731e177896ad
kdsh627 <105626103+kdsh627@users.noreply.github.com>
2025-11-11 12:42:02 +0900

    [Feat] 비버롹스 빌드본

--- NineReincarnation/Assets/08. Scripts/Map/Continue.cs
+++ NineReincarnation/Assets/08. Scripts/Map/Continue.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class Continue : MonoBehaviour
 {
+    public void Next()
+    {
+        SceneEventHandler.SceneStateChangedAndLoadScenes_Invoke(SceneDataManager.Instance.StoryCoreScene, SceneDataManager.Instance.StoryCoreScene, SceneDataManager.Instance.GetStorySubScene(1));
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
