a90eec3d6f35226a85076e29294e6f51e07ce20c
kdsh627 <105626103+kdsh627@users.noreply.github.com>
2025-11-25 14:35:36 +0900

    [Chore] 튜토리얼 이미지 추가

--- NineReincarnation/Assets/06. Scenes/Stages/New1/MileStone.cs
+++ NineReincarnation/Assets/06. Scenes/Stages/New1/MileStone.cs
@@ -1,21 +1,47 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UIElements;
+using static UnityEngine.Rendering.DebugUI;
 
 public class MileStone : MonoBehaviour
 {
-    [SerializeField] private Color32 color;
+    [SerializeField] private Color32 _color;
     [SerializeField] private SpriteRenderer _spriteRenderer;
     [SerializeField] private Renderer _renderer;
-    private MaterialPropertyBlock _propBlock;
-    private void OnValidate()
-    {
-        _spriteRenderer.color = color;
-    }
+    [SerializeField] private GameObject _panel;
 
+    private MaterialPropertyBlock _propBlock;
+    private Vector2               _defaultScale;
+    private string                _palyerTag = "player";
+    private Tween                 _tween;
+    private Vector2               _targetScale = Vector2.zero;
     private void Awake()
     {
+        _defaultScale = _panel.transform.localScale;
+
         _propBlock = new MaterialPropertyBlock();
-        SetColor(color);
+        SetColor(_color);
+
+        _panel.transform.localScale = _targetScale;
+    }
+
+    private void OnDisable()
+    {
+        _tween.Kill();
+    }
+
+    private void OpenPanel()
+    {
+        _panel.SetActive(true);
+
+        _tween = _panel.transform.DOScale(_defaultScale, 0.5f).SetEase(Ease.OutBack);
+    }
+
+    private void ClosePanel()
+    {
+        _tween = _panel.transform.DOScale(_targetScale, 1f).SetEase(Ease.InBack);
+
+        _panel.SetActive(false);
     }
 
     public void SetColor(Color32 newColor)
@@ -25,5 +51,21 @@ public class MileStone : MonoBehaviour
         _propBlock.SetColor("_Color", newColor);
 
         _renderer.SetPropertyBlock(_propBlock);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag(_palyerTag))
+        {
+            OpenPanel();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag(_palyerTag))
+        {
+            ClosePanel();
+        }
     }
 }
