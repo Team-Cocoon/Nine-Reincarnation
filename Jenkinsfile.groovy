d97523f137c87f513c16b56b5be23e5f4dfa080a
kdsh627 <105626103+kdsh627@users.noreply.github.com>
2026-01-03 01:28:00 +0900

    [Feat] 빌드 자동화 테스트 - 10

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -96,56 +96,56 @@ pipeline
         }
     }
 
-    post 
-    {
-        success 
-        {
-            script 
-            {
-                // --- 1. 변수 계산 (여기서 먼저 계산합니다) ---
-                def commitMsg = "변경 사항 없음 (수동 빌드 또는 재시도)"
-                def commitAuthor = "Unknown"
+    // post 
+    // {
+    //     success 
+    //     {
+    //         script 
+    //         {
+    //             // --- 1. 변수 계산 (여기서 먼저 계산합니다) ---
+    //             def commitMsg = "변경 사항 없음 (수동 빌드 또는 재시도)"
+    //             def commitAuthor = "Unknown"
                 
-                // changeSets가 리스트라 복잡해서 안전하게 가져오는 로직
-                def changeLogSets = currentBuild.changeSets
-                if (changeLogSets.size() > 0) {
-                    def entries = changeLogSets[0].items
-                    if (entries.length > 0) {
-                        def lastEntry = entries[entries.length - 1]
-                        commitMsg = lastEntry.msg
-                        commitAuthor = lastEntry.author.fullName
-                    }
-                }
+    //             // changeSets가 리스트라 복잡해서 안전하게 가져오는 로직
+    //             def changeLogSets = currentBuild.changeSets
+    //             if (changeLogSets.size() > 0) {
+    //                 def entries = changeLogSets[0].items
+    //                 if (entries.length > 0) {
+    //                     def lastEntry = entries[entries.length - 1]
+    //                     commitMsg = lastEntry.msg
+    //                     commitAuthor = lastEntry.author.fullName
+    //                 }
+    //             }
 
-                // --- 2. 디스코드 전송 (같은 블록 안이라 변수 사용 가능) ---
-                withCredentials([string(credentialsId: 'Discord-Webhook', variable: 'DISCORD')]) 
-                {
-                    discordSend description: """
-                    **결과** : ${currentBuild.result}
-                    📝 **커밋**: ${commitMsg}
-                    👤 **작성자**: ${commitAuthor}
-                    📅 **일시**: ${buildTime}
-                    ⏱ **실행 시간** : ${currentBuild.duration / 1000}s
-                    """,
-                    link: env.BUILD_URL, 
-                    result: currentBuild.currentResult, 
-                    title: "✅ ${env.JOB_NAME} 빌드 완료", 
-                    webhookURL: "$DISCORD"
-                }
-            }
-        }
+    //             // --- 2. 디스코드 전송 (같은 블록 안이라 변수 사용 가능) ---
+    //             withCredentials([string(credentialsId: 'Discord-Webhook', variable: 'DISCORD')]) 
+    //             {
+    //                 discordSend description: """
+    //                 **결과** : ${currentBuild.result}
+    //                 📝 **커밋**: ${commitMsg}
+    //                 👤 **작성자**: ${commitAuthor}
+    //                 📅 **일시**: ${buildTime}
+    //                 ⏱ **실행 시간** : ${currentBuild.duration / 1000}s
+    //                 """,
+    //                 link: env.BUILD_URL, 
+    //                 result: currentBuild.currentResult, 
+    //                 title: "✅ ${env.JOB_NAME} 빌드 완료", 
+    //                 webhookURL: "$DISCORD"
+    //             }
+    //         }
+    //     }
 
-        failure {
-            withCredentials([string(credentialsId: 'Discord-Webhook', variable: 'DISCORD')]) {
-                        discordSend description: """
-                        **결과** : ${currentBuild.result}
-                        📅 **일시**: ${buildTime}
-                        ⏱ **실행 시간** : ${currentBuild.duration / 1000}s
-                        """,
-                        link: env.BUILD_URL, result: currentBuild.currentResult, 
-                        title: "${env.JOB_NAME} : ${currentBuild.displayName} 실패", 
-                        webhookURL: "$DISCORD"
-            }
-        }
-    }
+    //     failure {
+    //         withCredentials([string(credentialsId: 'Discord-Webhook', variable: 'DISCORD')]) {
+    //                     discordSend description: """
+    //                     **결과** : ${currentBuild.result}
+    //                     📅 **일시**: ${buildTime}
+    //                     ⏱ **실행 시간** : ${currentBuild.duration / 1000}s
+    //                     """,
+    //                     link: env.BUILD_URL, result: currentBuild.currentResult, 
+    //                     title: "${env.JOB_NAME} : ${currentBuild.displayName} 실패", 
+    //                     webhookURL: "$DISCORD"
+    //         }
+    //     }
+    // }
 }
