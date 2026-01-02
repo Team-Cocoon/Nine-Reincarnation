d8ca60d35ec156d095d2e77c2e60a2e28e8f8f89
kdsh627 <105626103+kdsh627@users.noreply.github.com>
2026-01-03 02:16:32 +0900

    [Feat] 빌드 자동화 테스트 - 13

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,7 +8,7 @@ pipeline
 
     environment
     {
-        PROJECT_PATH = "${CUSTOM_WORKSPACE}\\${PROJECT_NAME}"
+        PROJECT_PATH = "C:\\Git\\Nine-Reincarnation\\${PROJECT_NAME}"
 
         OUTPUT_WIN = "C:\\Builds\\NineReincarnation\\Window\\NineReincarnation.exe"
         OUTPUT_WEB = "C:\\Builds\\NineReincarnation\\Web"
